349f23cf6dac768198b6e97ca5cd096c982338a2
changhechen <66896267+changhechen@users.noreply.github.com>
2026-04-25 14:54:02 +0000

    Deploying to gh-pages from @ changhechen/changhechen.github.io@93997ded1ee62451720dfffee5931448502377a5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AisenGinn/AisenGinn.github.io",
+    "data-repo": "changhechen/changhechen.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
